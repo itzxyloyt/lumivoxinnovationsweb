94bfe77641af23516ed599373dff4994af537ea8
Xylo <82177322+itzxyloyt@users.noreply.github.com>
2024-06-06 07:12:33 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,35 @@
 // @ts-check
- 
+/** @type {import('next').NextConfig} */
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-const experimental.instrumentationHook = true;
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    if (cfg.cache && !dev) {
+      cfg.cache = Object.freeze({
+        type: 'memory',
+      })
+      cfg.cache.maxMemoryGenerations = 0
+    }
+    return config
+  },
+}
  
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
-      /* development only config options here */
+      /* development config options */
     }
   }
  
   return {
-    /* config options for all phases except development here */
+   experimental.instrumentationHook: true,
+   productionBrowserSourceMaps: false,
+   experimental.serverSourceMaps: false,
+   experimental.webpackBuildWorker: true,
   }
 }
+
+export default nextConfig
