da40c1dfbc10aea8b83797993ffc0528d073236f
Xylo <82177322+itzxyloyt@users.noreply.github.com>
2024-06-06 06:55:32 -0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,16 @@
+import { Suspense } from 'react'
+import { Assets } from './Components'
+ 
+export default function Posts() {
+  return (
+    <section>
+      <Suspense fallback={<p>Loading assets...</p>}>
+        <Assets />
+      </Suspense>
+    </section>
+  )
+}
+
 export default function Page() {
   return <h1>Hello, Dashboard Page!</h1>
 }
