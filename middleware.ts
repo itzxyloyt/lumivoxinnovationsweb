033288560de0b13981f0c00e96acf83fbc7d91d4
Xylo <82177322+itzxyloyt@users.noreply.github.com>
2024-06-06 07:15:34 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,17 @@
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+  const currentUser = request.cookies.get('currentUser')?.value
+ 
+  if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
+    return Response.redirect(new URL('/dashboard', request.url))
+  }
+ 
+  if (!currentUser && !request.nextUrl.pathname.startsWith('/login')) {
+    return Response.redirect(new URL('/login', request.url))
+  }
+}
+ 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
- 
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      has: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
- 
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      has: [{ type: 'header', key: 'x-present' }],
-      missing: [{ type: 'header', key: 'x-missing', value: 'prefetch' }],
-    },
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
